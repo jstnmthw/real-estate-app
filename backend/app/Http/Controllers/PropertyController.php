1d50928ae6c36fa933e71f592809785ccbbd5556
Justin <web@jstn.ly>
2022-12-29 00:54:03 +0700

    added: Basic property search using MeiliSearch

--- backend/app/Http/Controllers/PropertyController.php
+++ backend/app/Http/Controllers/PropertyController.php
@@ -6,8 +6,11 @@ use App\Models\Property;
 use App\Http\Resources\PropertyResource;
 use App\Http\Requests\StorePropertyRequest;
 use App\Http\Requests\UpdatePropertyRequest;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Http\Request;
+use Meilisearch\Endpoints\Indexes;
 
 class PropertyController extends Controller
 {
@@ -19,6 +22,27 @@ class PropertyController extends Controller
     public function index()
     {
         return PropertyResource::collection(Property::paginate());
+    }
+
+    /**
+     * Search within meilisearch
+     *
+     * @return LengthAwarePaginator
+     */
+    public function search(Request $request)
+    {
+        return Property::search(
+            query: trim($request->get('q', '')),
+            callback: function (Indexes $meilisearch, string $query, array $options) use ($request) {
+                if ($request->has('bedrooms')) {
+                    $options['filter'] = "bedrooms = {$request->get('bedrooms')}";
+                }
+                return $meilisearch->search(
+                    query: $query,
+                    options: $options,
+                );
+            },
+        )->paginate();
     }
 
     /**

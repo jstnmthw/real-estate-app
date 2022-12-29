86aa1cd583941d16369de1019c951c2262017664
Justin <web@jstn.ly>
2022-12-29 21:14:40 +0700

    update: Property.php include other query strings

--- backend/app/Http/Controllers/PropertyController.php
+++ backend/app/Http/Controllers/PropertyController.php
@@ -43,7 +43,7 @@ class PropertyController extends Controller
                         $options,
                     );
                 },
-            )->paginate()
+            )->paginate()->withQueryString()
         );
     }
 

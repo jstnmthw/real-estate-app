0d9500f75972d0256ada536118fbadccf681de14
Justin <web@jstn.ly>
2023-01-07 23:19:10 +0700

    update: tailwind.config.js color

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,22 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    '!./node_modules',
+  ],
   theme: {
     extend: {
       colors: {
-        blue: {
-          DEFAULT: '#0973FF',
-          50: '#C1DBFF',
-          100: '#ACD0FF',
-          200: '#83B9FF',
-          300: '#5BA1FF',
-          400: '#328AFF',
-          500: '#0063E6',
-          600: '#005EDB',
-          700: '#004DB3',
-          800: '#002960',
-          900: '#001128',
+        lavender: {
+          50: '#edf2ff',
+          100: '#dee8ff',
+          200: '#c3d4ff',
+          300: '#9fb7ff',
+          400: '#798eff',
+          500: '#5f6dfb',
+          600: '#3b3df0',
+          700: '#2f2ed4',
+          800: '#2829ab',
+          900: '#282b87',
         },
       },
       fontFamily: {

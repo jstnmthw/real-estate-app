b95c0dd8990e4cdbc49514b0c5fe55e065b65f6f
Justin <web@jstn.ly>
2023-01-09 23:45:10 +0700

    updates: generating Model types

--- backend/app/Models/UserSocialLogin.php
+++ backend/app/Models/UserSocialLogin.php
@@ -2,21 +2,38 @@
 
 namespace App\Models;
 
-use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Carbon;
 
 /**
+ * App\Models\UserSocialLogin
+ *
  * @property int $id
  * @property int $user_id
- * @property int $external_id
+ * @property string $external_id
  * @property string $access_token
- * @property string $refresh_token
- * @property Carbon $created_at
- * @property Carbon $updated_at
- * @property-read User $user
- * @package App
+ * @property string|null $refresh_token
+ * @property array|null $scopes
+ * @property Carbon|null $expires
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read User|null $user
+ * @method Builder|UserSocialLogin newModelQuery()
+ * @method Builder|UserSocialLogin newQuery()
+ * @method Builder|UserSocialLogin query()
+ * @method Builder|UserSocialLogin whereAccessToken($value)
+ * @method Builder|UserSocialLogin whereCreatedAt($value)
+ * @method Builder|UserSocialLogin whereExpires($value)
+ * @method Builder|UserSocialLogin whereExternalId($value)
+ * @method Builder|UserSocialLogin whereId($value)
+ * @method Builder|UserSocialLogin whereRefreshToken($value)
+ * @method Builder|UserSocialLogin whereScopes($value)
+ * @method Builder|UserSocialLogin whereUpdatedAt($value)
+ * @method Builder|UserSocialLogin whereUserId($value)
+ * @mixin Builder
  */
 class UserSocialLogin extends Model
 {

f2893631c42d1de111a0a36e5fe330d6947321af
Justin <web@jstn.ly>
2023-01-09 21:25:00 +0700

    updates: rethinking loading inside Card.tsx

--- frontend/components/Card.tsx
+++ frontend/components/Card.tsx
@@ -12,8 +12,7 @@ import { Property } from '@/types/property';
 const Card: FC<{
   property: Property;
   className?: string;
-  loading?: boolean;
-}> = ({ className, property, loading = false }) => {
+}> = ({ className, property }) => {
   return (
     <div
       className={classNames(
@@ -21,48 +20,42 @@ const Card: FC<{
         'overflow-hidden rounded-xl bg-white shadow-lg',
       )}
     >
-      {loading ? (
-        <div>Loading</div>
-      ) : (
-        <>
-          <Image
-            width={500}
-            height={500}
-            src={property?.image}
-            alt={''}
-            className="object-cover"
-          />
-          <div className="relative p-6">
-            <button
-              className="absolute top-4 right-4 rounded-lg border border-zinc-200 bg-white p-2 text-zinc-500 shadow hover:text-pink-500"
-              type="button"
-              title="Favorite this property"
-            >
-              <span className="sr-only">Favorite this property</span>
-              <HeartIcon className="h-4 w-4" />
-            </button>
-            <span className="text-base font-semibold text-lavender-500 md:text-lg">
-              {property?.currency}
-              {property?.sales_price?.toLocaleString()}
-            </span>
-            <h2 className="font-semibold md:text-lg">{property?.title}</h2>
-            <p className="mb-4 text-sm font-medium text-zinc-500">
-              {property?.address}
-            </p>
-            <div className="flex justify-between text-sm md:text-base">
-              <div className="flex items-center font-medium">
-                <BedIcon className="mr-2 h-5 w-5" /> {property.bedrooms} Bed
-              </div>
-              <div className="flex items-center font-medium">
-                <BathIcon className="mr-2 h-5 w-5" /> {property.bathrooms} Bath
-              </div>
-              <div className="flex items-center font-medium">
-                {property.area_size?.toLocaleString()} {property.area_type}
-              </div>
-            </div>
+      <Image
+        width={500}
+        height={500}
+        src={property?.image}
+        alt={''}
+        className="object-cover"
+      />
+      <div className="relative p-6">
+        <button
+          className="absolute top-4 right-4 rounded-lg border border-zinc-200 bg-white p-2 text-zinc-500 shadow hover:text-pink-500"
+          type="button"
+          title="Favorite this property"
+        >
+          <span className="sr-only">Favorite this property</span>
+          <HeartIcon className="h-4 w-4" />
+        </button>
+        <span className="text-base font-semibold text-lavender-500 md:text-lg">
+          {property?.currency}
+          {property?.sales_price?.toLocaleString()}
+        </span>
+        <h2 className="font-semibold md:text-lg">{property?.title}</h2>
+        <p className="mb-4 text-sm font-medium text-zinc-500">
+          {property?.address}
+        </p>
+        <div className="flex justify-between text-sm md:text-base">
+          <div className="flex items-center font-medium">
+            <BedIcon className="mr-2 h-5 w-5" /> {property.bedrooms} Bed
           </div>
-        </>
-      )}
+          <div className="flex items-center font-medium">
+            <BathIcon className="mr-2 h-5 w-5" /> {property.bathrooms} Bath
+          </div>
+          <div className="flex items-center font-medium">
+            {property.area_size?.toLocaleString()} {property.area_type}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };

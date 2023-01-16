6cc100ee7c1cd6354b73c014bfb84b3050b3d42d
Justin <web@jstn.ly>
2023-01-16 20:39:08 +0700

    updates: root layout.tsx with i18n

--- frontend/app/[lang]/layout.tsx
+++ frontend/app/[lang]/layout.tsx
@@ -1,15 +1,26 @@
 import '@/app/globals.css';
 import { ReactNode } from 'react';
 import { Inter } from '@next/font/google';
+import { i18n } from '@/i18n/confg';
+
+export async function generateStaticParams() {
+  return i18n.locales.map((locale) => ({ lang: locale }));
+}
 
 const inter = Inter({
   variable: '--font-inter',
   subsets: ['latin'],
 });
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+export default function RootLayout({
+  children,
+  params,
+}: {
+  children: ReactNode;
+  params: { lang: string };
+}) {
   return (
-    <html lang="en" className={inter.className}>
+    <html lang={params.lang} className={inter.className}>
       <body className="relative">{children}</body>
     </html>
   );

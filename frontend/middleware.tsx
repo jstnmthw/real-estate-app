ddce3f5dd88e5fc86ccaf5e3d831e17263cdc5b5
Justin <web@jstn.ly>
2023-01-16 20:39:42 +0700

    adds: i18n middleware.tsx

--- frontend/middleware.tsx
+++ frontend/middleware.tsx
@@ -1,41 +1,48 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
-import { i18n } from '@/i18n/confg'
+import { i18n } from '@/i18n/confg';
 
-import { match as matchLocale } from '@formatjs/intl-localematcher'
-import Negotiator from 'negotiator'
+import { match as matchLocale } from '@formatjs/intl-localematcher';
+import Negotiator from 'negotiator';
 
 function getLocale(request: NextRequest): string | undefined {
   // Negotiator expects plain object so we need to transform headers
-  const negotiatorHeaders: Record<string, string> = {}
-  request.headers.forEach((value, key) => (negotiatorHeaders[key] = value))
+  const negotiatorHeaders: Record<string, string> = {};
+  request.headers.forEach((value, key) => (negotiatorHeaders[key] = value));
 
   // Use negotiator and intl-locale matcher to get best locale
-  let languages = new Negotiator({ headers: negotiatorHeaders }).languages()
+  let languages = new Negotiator({ headers: negotiatorHeaders }).languages();
   // @ts-ignore locales are readonly
-  const locales: string[] = i18n.locales
-  return matchLocale(languages, locales, i18n.defaultLocale)
+  const locales: string[] = i18n.locales;
+  return matchLocale(languages, locales, i18n.defaultLocale);
 }
 
 export function middleware(request: NextRequest) {
   // Skip next internal requests
-  if (request.nextUrl.pathname.startsWith('/_next')) return
+  if (request.nextUrl.pathname.startsWith('/_next')) return;
 
   // Check if there is any supported locale in the pathname
-  const pathname = request.nextUrl.pathname
+  const pathname = request.nextUrl.pathname;
   const pathnameIsMissingLocale = i18n.locales.every(
-    (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
-  )
+    (locale) =>
+      !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`,
+  );
 
   // Let's redirect if there is no locale
   if (pathnameIsMissingLocale) {
-    const locale = getLocale(request)
-    return NextResponse.redirect(new URL(`/${locale}/${pathname}`, request.url))
+    const locale = getLocale(request);
+    return NextResponse.redirect(
+      new URL(`/${locale}/${pathname}`, request.url),
+    );
   }
 }
 
 export const config = {
   // We can enable redirect just from root
-  // matcher: "/"
-}
+  // matcher: '/',
+  matcher: [
+    // Skip all internal paths (_next)
+    '/((?!_next).*)',
+  ],
+};

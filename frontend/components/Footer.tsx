19b0ea960325f0a7aa03373ffba28757481f98a9
Justin <web@jstn.ly>
2023-03-12 17:47:01 +0700

    refactor: footer container

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -97,7 +97,7 @@ export default function Footer() {
       <h2 id="footer-heading" className="sr-only">
         Footer
       </h2>
-      <div className="mx-auto max-w-7xl py-12 px-6 lg:py-16 lg:px-8">
+      <div className="container mx-auto py-12 lg:py-16">
         <div className="pb-8 xl:grid xl:grid-cols-5 xl:gap-8">
           <div className="grid grid-cols-2 gap-8 xl:col-span-4">
             <div className="md:grid md:grid-cols-2 md:gap-8">
